c28390c74cfa6ee14992fa65d62295001a35f80c
Cheryn1988 <cherluza@gmail.com>
2023-09-15 17:00:35 +0100

    criação caixa com msg de boas vindas ao usuário

--- port.js
+++ port.js
@@ -1,2 +1,4 @@
+//criação de caixa que solicita nome do usuário e fornece aviso de boas vindas
 let usuario = prompt("Qual é o seu nome?");
+alert(`${usuario}, seja bem vindo(a) ao meu Portfólio. Sinta-se à vontade!`)
 console.log(usuario)
